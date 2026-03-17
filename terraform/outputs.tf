e92b56b709a53b7e8c778a6ba8af704bba1bd79c
damihubs <damiakinkuolie@gmail.com>
2026-03-16 22:17:08 -0500

    Remove unnecessary header from outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,3 @@
-terraform — terraform/outputs.tf
 output "rg_name"     { value = azurerm_resource_group.rg.name }
 output "webapp_name" { value = azurerm_linux_web_app.webapp.name }
 output "webapp_url"  { value = "https://${azurerm_linux_web_app.webapp.default_hostname}" }
