19f6bc66d65dd9c34572389356ac8e9e287bd9f6
damihubs <damiakinkuolie@gmail.com>
2026-03-16 22:15:38 -0500

    Add Azure resources for web app deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,30 @@ resource "random_string" "suffix" {
 }
 
 locals {
+
+  location      = "East US"
+  app_base_name = "lesson6-html-app"
+  app_name      = "${local.app_base_name}-${random_string.suffix.result}"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "lesson6-rg"
+  location = local.location
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "lesson6-service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                = local.app_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
+  site_config { app_command_line = "" }
+}
+
